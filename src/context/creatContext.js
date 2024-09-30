7be684b3ca17584351050bd9d78129cb392a133b
shuvajit gayen <shuvajitgayen57@gmail.com>
2024-09-30 10:28:27 +0530

    create a context that fatch all message

---
+++ src/context/creatContext.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import supabase from "@/supabaseClient";
+
+function CreatContext() {
+    const [messages, setMessage] = useState([]);
+    useEffect(() => {
+        const fetchmessage = async () => {
+            const { data, error } = await supabase
+                .from('chat')
+                .select()
+                .range(0, messages.length)
+                .order('id', { ascending: false });
+            if (error) {
+                console.error('Error fetching message:', error);
+            } else {
+                await setMessage(data);
+            }
+        }
+        fetchmessage();
+        
+    })
+
+
+    return{
+        messages
+    }
+
+
+}
+
+export default CreatContext
+
+
